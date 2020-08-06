14ccc0038a2aa773686fdf45aa68c81940fd500b
Aniket <anikettikone9@gmail.com>
2020-08-06 21:36:43 +0530

    Updates

---
+++ precache-manifest.16b32e3c71dc6323457d88771d047276.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5bcbf543ed93200eb519a9acac2dfa",
+    "url": "/calculator-using-react/index.html"
+  },
+  {
+    "revision": "e06b9667145ca66d7e61",
+    "url": "/calculator-using-react/static/css/main.44c761aa.chunk.css"
+  },
+  {
+    "revision": "d78d0983e51569317692",
+    "url": "/calculator-using-react/static/js/2.b3422196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-using-react/static/js/2.b3422196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06b9667145ca66d7e61",
+    "url": "/calculator-using-react/static/js/main.670e738e.chunk.js"
+  },
+  {
+    "revision": "69039d561ca0a5575826",
+    "url": "/calculator-using-react/static/js/runtime-main.eb4df07e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator-using-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
